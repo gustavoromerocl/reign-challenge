4cab1e097651812a03104b6961fcdba561472a59
romerodevelopments <gandresrp@gmail.com>
2022-05-16 01:42:34 -0400

    funcionalidad - agregar indicador de carga y manejo de errores

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Card from '../components/Card';
+import Spinner from '../components/Spinner';
 /* Importamos la store de zustand */
 import useNewsStore from '../zustand/news-store';
 
@@ -27,12 +28,13 @@ function Home(props) {
   return (
     <div className={`home-container ${props.className}`}>
       <div className='cards-container'>
-        
+        {isFetchingNews && <Spinner />}
         {/*Se mapea el array con la información de la api para contruir las cards con la info*/
+          
           news.map(({ author, story_title, story_url, created_at, created_at_i }) =>
             <Card author={author} storyTitle={story_title} storyUrl={story_url} createdAt={created_at} key={created_at_i.toString()} />)
         }
-
+        {fetchNewsError && <div>Error 404 Not found</div>}
         {/*  <Dropdown /> */}
 
       </div>
